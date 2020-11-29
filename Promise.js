86f6336ab572f444f553fe50cf0ed9e9fedc1153
bysking <bysking@qq.com>
2020-11-29 13:02:50 +0800

    1.4-处理异步，先订阅再触发执行

--- Promise.js
+++ Promise.js
@@ -8,10 +8,18 @@ class Promise {
         this.value = undefined;
         this.reason = undefined;
 
+        this.onfulfilledArr = [];
+        this.onrejectArr = [];
+
         let resolve = (value) => {
             if (this.status === PENDING) { // 状态pending才能修改状态
                 this.value = value;
                 this.status = RESOLVED;
+                
+                // 订阅列表执行
+                this.onfulfilledArr.forEach(fn => { // 依次取出执行
+                    fn()
+                })
             }
         }
         let reject = (reason) => {
@@ -19,6 +27,11 @@ class Promise {
             if (this.status === PENDING) {
                 this.reason = reason;
                 this.status = REJECTED
+
+                // 通知订阅者执行
+                this.onrejectArr.forEach(fn => {// 依次取出执行
+                    fn();
+                })
             }
         }
 
@@ -36,6 +49,18 @@ class Promise {
 
         if (this.status === REJECTED) {
             onreject && onreject(this.reason)
+        }
+
+        if (this.status === PENDING) {
+
+            // 如果是异步，就先订阅好
+            onfulfilled && this.onfulfilledArr.push(() => {// 这样套一层方便做切片加入自定义逻辑
+                onfulfilled(this.value)
+            });
+
+            onreject && this.onrejectArr.push(() => { // 这样套一层方便做切片加入自定义逻辑
+                onreject(this.reason)
+            })
         }
     }
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let Promise = require('./Promise.js').Promise;
 let p = new Promise((resolve, reject) => {
     // resolve('执行成功')
-    reject('执行失败')
+    setTimeout(() => {
+        reject('执行失败')
+    }, 3000)
 }).then((data) => {
     console.log(data);
 }, (err) => {
